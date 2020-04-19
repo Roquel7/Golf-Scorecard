a6bda88c857786bb2a7a076e33e3f7c56c6b67f3
Roquel7 <Roquel7@github.com>
2020-04-18 20:44:39 -0600

    initial commit

---
+++ js/golfScore.js
@@ -0,0 +1,11 @@
+// let playerSlots = 4;
+
+// function golfCard() {
+//     //creating the holes 
+//     for (let holes = 1; holes <= 23; holes++) {
+//         // document.getElementById('holeNumber').append("<div id='hole"+ holes + "'></div>")
+//         $("#holeNumber").append("<div id='hole'>holes" +holes+ "</div>");
+//     }
+
+    
+// }
