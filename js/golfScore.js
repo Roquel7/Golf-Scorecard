7e9ef5ebce7975604e0bff6c0537a4199422d40c
Roquel7 <Roquel7@github.com>
2020-04-22 00:48:47 -0600

    added name rows with number cells

--- js/golfScore.js
+++ js/golfScore.js
@@ -8,37 +8,128 @@ function golfCard() {
         $("#firstRow").append("<td>" + i + "</td>")
     }
     $("#firstRow").append("<td>IN</td> <td>TOTAL</td>")
-    $("#myTable").append("<tr id='secondRow'> <th></th> </tr>")
-    $("#myTable").append("<tr id='thirdRow'> <th></th> </tr>")
-    $("#myTable").append("<tr id='fourthRow'> <th></th> </tr>")
 
-    rowTwo();
-    rowThree();
-    rowFour();
+    $("#myTable").append("<tr id='secondRow'> <th></th> </tr>")
+
+    secondRow();
+    thirdRow();
+    fourRow();
+    fifthRow();
+
+    $("#myTable").append("<tr id='sixthRow'> <th> <input class='names firstName' type='text' placeholder='1st Name'> </th> </tr>")
+    sixthRow();
+
+    $("#myTable").append("<tr id='seventhRow'> <th> <input class='names firstName' type='text' placeholder='2nd Name'> </th> </tr>")
+    seventhRow();
+
+    $("#myTable").append("<tr id='eightRow'> <th> <input class='names firstName' type='text' placeholder='3rd Name'> </th> </tr>")
+    eightRow();
+
+    $("#myTable").append("<tr id='ninthRow'> <th> <input class='names firstName' type='text' placeholder='4th Name'> </th> </tr>")
+    ninthRow();
 }
 
-function rowTwo() {
-    document.getElementById('secondRow');
-    for (let i = 1; i <= 20; i++) {
+function secondRow() {
+
+    for (let i = 1; i <= 21; i++) {
         $("#secondRow").append("<td></td>")
     }
 }
 
-function rowThree() {
-    document.getElementById('thirdRow');
-    for (let i = 1; i <= 20; i++) {
+function thirdRow() {
+
+    let third = document.createElement("tr")
+    let thirdHeader = document.createElement("th")
+    third.appendChild(thirdHeader);
+    third.id = "thirdRow";
+    $("#myTable").append(third)
+    
+    for (let i = 1; i <= 21; i++) {
         $("#thirdRow").append("<td></td>")
     }
+    $("#myTable").append(third);
 }
 
-function rowFour() {
-    document.getElementById('fourthRow');
-    for (let i = 1; i <= 20; i++) {
+function fourRow() {
+
+    let fourth = document.createElement("tr")
+    let fourthHeader = document.createElement("th")
+    fourth.appendChild(fourthHeader);
+    fourth.id = "fourthRow";
+    $("#myTable").append(fourth)
+    
+    for (let i = 1; i <= 21; i++) {
         $("#fourthRow").append("<td></td>")
     }
+    $("#myTable").append(fourth);
 }
 
+function fifthRow() {
 
+    let fifth = document.createElement("tr")
+    let fifthHeader = document.createElement("th")
+    fifthHeader.innerText = "Handicap";
+    fifth.appendChild(fifthHeader);
+    fifth.id = "fifthRow";
+    $("#myTable").append(fifth)
+    
+    for (let i = 1; i <= 21; i++) {
+        $("#fifthRow").append("<td></td>")
+    }
+    $("#myTable").append(fifth);
+} 
+
+function sixthRow() {
+
+    for (let i = 1; i <= 9; i++) {
+        $("#sixthRow").append("<td> <input class='numberCells' type='number' min='1' max='99'> </td>")
+    }
+    $("#sixthRow").append("<td></td> <td></td>")
+    for (let i = 9; i <=17 ; i++) {
+        $("#sixthRow").append("<td> <input class='numberCells' type='number' min='1' max='99'> </td>")
+    }
+    $("#sixthRow").append("<td></td> <td></td>")
+
+}
+
+function seventhRow() {
+
+    for (let i = 1; i <= 9; i++) {
+        $("#seventhRow").append("<td> <input class='numberCells' type='number' min='1' max='99'> </td>")
+    }
+    $("#seventhRow").append("<td></td> <td></td>")
+    for (let i = 9; i <=17 ; i++) {
+        $("#seventhRow").append("<td> <input class='numberCells' type='number' min='1' max='99'> </td>")
+    }
+    $("#seventhRow").append("<td></td> <td></td>")
+
+}
+
+function eightRow() {
+
+    for (let i = 1; i <= 9; i++) {
+        $("#eightRow").append("<td> <input class='numberCells' type='number' min='1' max='99'> </td>")
+    }
+    $("#eightRow").append("<td></td> <td></td>")
+    for (let i = 9; i <=17 ; i++) {
+        $("#eightRow").append("<td> <input class='numberCells' type='number' min='1' max='99'> </td>")
+    }
+    $("#eightRow").append("<td></td> <td></td>")
+
+}
+
+function ninthRow() {
+
+    for (let i = 1; i <= 9; i++) {
+        $("#ninthRow").append("<td> <input class='numberCells' type='number' min='1' max='99'> </td>")
+    }
+    $("#ninthRow").append("<td></td> <td></td>")
+    for (let i = 9; i <=17 ; i++) {
+        $("#ninthRow").append("<td> <input class='numberCells' type='number' min='1' max='99'> </td>")
+    }
+    $("#ninthRow").append("<td></td> <td></td>")
+
+}
 
 
 // function Rows() {
